baf6cdb7b19b22303ecc316666f8a6a18de681ef
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-09-12 08:46:00 +0900

    adding CORS k8s-node2

--- src/main/java/com/example/book/config/SecurityConfig.java
+++ src/main/java/com/example/book/config/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // 3 override
         configuration.addAllowedOrigin("http://localhost:80");
         configuration.addAllowedOrigin("http://192.168.56.33");
         configuration.addAllowedOrigin("http://192.168.56.33:80");
-        configuration.addAllowedOrigin("http://192.168.56.31:30004");
+        configuration.addAllowedOrigin("http://192.168.56.31:30004"); // k8s-node1
+        configuration.addAllowedOrigin("http://192.168.56.32:30004"); // k8s-node2
         configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedOrigin("http://localhost:3001");
         configuration.addAllowedOrigin("http://13.124.143.225");
